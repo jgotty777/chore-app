eb8e009d8e05b0976cce501910a5f8b4236da682
jgottweis <jgottweis@gmail.com>
2025-05-30 14:07:14 -0400

    add test so GHA isnt mad

---
+++ microservices/auth/src/auth.controller.spec.ts
@@ -0,0 +1,22 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+describe('AppController', () => {
+  let appController: AuthController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile();
+
+    appController = app.get<AuthController>(AuthController);
+  });
+
+  describe('root', () => {
+    it('should return true if user authenticates"', async () => {
+      expect( await appController.signIn({email:'test', password: 'test'})).toBe(true);
+    });
+  });
+});
