a68311501966a8ac3136afa95ad5e6cedf381236
jgottweis <jgottweis@gmail.com>
2025-10-03 14:16:44 -0400

    playwright changes

--- apps/web/playwright/index.tsx
+++ apps/web/playwright/index.tsx
@@ -1,2 +1,22 @@
 // Import styles, initialize component theme here.
 // import '../src/common.css';
+// playwright/index.ts
+import '@mantine/core/styles.css';
+import { ColorSchemeScript, mantineHtmlProps, MantineProvider } from '@mantine/core';
+import { SessionProvider } from 'next-auth/react';
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </MantineProvider>
+      </body>
+    </html>
+  );
+}
